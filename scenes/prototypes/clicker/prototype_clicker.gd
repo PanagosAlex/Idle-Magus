a274ecf0757d6369bd09581a3a852af909625fca
Alexandros Panagos <panagosalex@gmail.com>
2025-10-23 00:08:34 +0300

    U/I prototype
    Creating a prototype for U/I.

--- scenes/prototypes/clicker/prototype_clicker.gd
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -1,26 +1,19 @@
 class_name PrototypeClicker
 extends Control
-## A clicker prototype creating mana.
 
-## Reference to the label displaying current amount of mana.
 @export var label : Label
-# Current amount of mana.
+
 var mana : int = 0
 
-
-## Initializer
 func _ready() -> void:
 	update_label_text()
 
-## Create 1 mana.
 func create_mana() -> void:
 	mana += 1
 	update_label_text()
 
-## Update text of the label.
 func update_label_text() -> void:
 	label.text = "Mana : %s" %mana
 
-## Triggered when clicking on mana orb
 func _on_button_pressed() -> void:
 	create_mana()
